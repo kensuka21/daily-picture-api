88e0dfb528431c172d1e2d88da2820cacc6c9925
Kenji Mukai <kensuka21@gmail.com>
2017-12-11 19:38:34 -0400

    Refactor.

--- daily_picture/settings.py
+++ daily_picture/settings.py
@@ -71,14 +71,13 @@ CORS_ALLOW_CREDENTIALS = True
 ROOT_URLCONF = 'daily_picture.urls'
 
 JWT_AUTH = {
-    'JWT_VERIFY_EXPIRATION': False,
+    'JWT_VERIFY_EXPIRATION': False
 }
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

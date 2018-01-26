9bc03c13b8a19d8e3baa7fefd5f90a122c81f589
Kenji Mukai <kensuka21@gmail.com>
2018-01-25 20:47:58 -0400

    Removing some unusual imports.

--- login/views.py
+++ login/views.py
@@ -2,14 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib.auth import logout
-from django.shortcuts import render
 
-# Create your views here.
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
-
 from login.serializers import UserSerializer
 
 
